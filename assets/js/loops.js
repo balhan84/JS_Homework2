369eb00598343b658c2457ebb8f94a7e2252c3af
balhan84 <balamutenkoanna@gmail.com>
2024-05-15 16:11:27 +0300

    Add loops

---
+++ assets/js/loops.js
@@ -0,0 +1,80 @@
+//task #1
+//while
+// let startNumber = 25;
+// const endNumber = 0;
+
+// while (startNumber >= endNumber) {
+//   console.log(startNumber);
+//   startNumber--;
+// }
+
+//do...while
+
+// do {
+//   console.log(startNumber);
+//   startNumber--;
+// } while (startNumber >= endNumber);
+
+//for
+// for (startNumber; startNumber >= endNumber; startNumber--) {
+//   console.log(startNumber);
+// }
+
+//task#2
+//while
+// let startNumber = 10;
+// const endNumber = 50;
+
+// while (startNumber <= endNumber) {
+//   if (startNumber % 5 === 0) {
+//     console.log(startNumber);
+//   }
+//   startNumber++;
+// }
+
+//do..while
+
+// do {
+//   if (startNumber % 5 === 0) {
+//     console.log(startNumber);
+//   }
+//   startNumber++;
+// } while (startNumber <= endNumber);
+
+//for
+
+// for (startNumber; startNumber <= endNumber; startNumber++) {
+//   if (startNumber % 5 === 0) {
+//     console.log(startNumber);
+//   }
+// }
+
+//task#3
+
+//while
+
+// let startNumber = 1;
+// const endNumber = 100;
+// let i = startNumber;
+// let sum = 0;
+
+// while (i <= endNumber) {
+//   sum = sum + i;
+//   console.log(sum);
+//   i++;
+// }
+
+//do..while
+
+// do {
+//   sum = sum + i;
+//   console.log(sum);
+//   i++;
+// } while (i <= endNumber);
+
+//for
+// debugger;
+// for (i; i <= endNumber; i++) {
+//   sum = sum + i;
+//   console.log(sum);
+// }
